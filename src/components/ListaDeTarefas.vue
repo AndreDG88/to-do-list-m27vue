f7cdbc9e7c9e5428ba16e38214dd2c98d5f15d05
André Soares <andre.soares.designer@gmail.com>
2024-11-12 22:38:02 -0300

    alteração lista de tarefas

--- src/components/ListaDeTarefas.vue
+++ src/components/ListaDeTarefas.vue
@@ -2,7 +2,7 @@
 const props = defineProps(['tarefas'])
 </script>
 <template>
-    <ul class="list-group mt-4">
+    <ul class="list-group mt-4" v-if="props.tarefas.length > 0">
         <li class="list-group-item" v-for="tarefa in props.tarefas">
             <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
                 :id="tarefa.titulo" type="checkbox">
@@ -11,6 +11,7 @@ const props = defineProps(['tarefas'])
             </label>
         </li>
     </ul>
+    <p v-else class="mt-4">Não há tarefas pendentes.</p>
 </template>
 <style scoped>
 .done {
